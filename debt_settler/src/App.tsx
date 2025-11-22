4d78d35f8cebdcc339d83a8b36ac6c459f091e4d
Waleed <waleed.binkhalid@hotmail.com>
2025-11-22 07:14:47 +0100

    Improve UI

--- debt_settler/src/App.tsx
+++ debt_settler/src/App.tsx
@@ -37,25 +37,26 @@ function App() {
         Debt Settler
       </h1>
       <p className="text-center text-lg">Designed by Khadija Rehman and developed By WBK</p>
-      <AddUser users={users} setUsers={setUsers} />
-      <AddTransaction users={users} handleDelete={handleDelete} addTransaction={addTransaction} />
-
-      <div className="flex justify-center bg-base">
-        <Settle transactions={transactions} users={users} onSettle={handleSettle} />
-      </div>
-
-      <div className="flex flex-row justify-center items-start gap-10 px-5">
-        <div className="w-1/2 min-w-[350px]">
-          <h3 className="text-center text-white text-base">Original Transactions</h3>
-          <Graph users={users} transactions={transactions} immediateRender={true} />
+      <div style={{ display: 'flex', margin: '20px 0', padding: '0 20px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', margin: '30px 0', padding: '0 20px', width: '30%' }}>
+          <AddUser users={users} setUsers={setUsers} />
+          <AddTransaction users={users} handleDelete={handleDelete} addTransaction={addTransaction} />
+          <Settle transactions={transactions} users={users} onSettle={handleSettle} />
         </div>
 
-        <div className="w-1/2 min-w-[350px]">
-          <h3 className="text-center text-white text-base">Simplified Transactions</h3>
-          <Graph users={users} transactions={simplifiedTransactions} immediateRender={false} />
+        <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px 0', padding: '0 20px', width: '70%' }}>
+          <div style={{ width: '45%' }}>
+            <h3 className="text-center text-white text-base">Original Transactions</h3>
+            <Graph users={users} transactions={transactions} immediateRender={true} />
+          </div>
+
+          <div style={{ width: '45%' }}>
+            <h3 className="text-center text-white text-base">Simplified Transactions</h3>
+            <Graph users={users} transactions={simplifiedTransactions} immediateRender={false} />
+          </div>
         </div>
       </div>
-    </div >
+    </div>
 
 
   )
